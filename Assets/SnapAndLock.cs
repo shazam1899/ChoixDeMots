fd4a7d8a73179050571b755c1df9a7a7fb2fe901
LAUNAY Dylan Brassart <dylan.launay@brassart.fr>
2026-04-02 13:04:00 +0200

    Push 05 - Test Drag and drop
    Test du drag and drop qui fonctionne mais qui n'a pas de fonction de choix des objets pour l'instant

---
+++ Assets/SnapAndLock.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class SnapAndLock : MonoBehaviour
+{
+    public Transform snapPoint;
+
+    private void Start()
+    {
+        // On vérifie si l'objet est un bloc
+        {
+            // On récupère le script BlockLock sur l'objet
+            XRSocketInteractor interactable = GetComponent<XRSocketInteractor>();
+                interactable.selectExited.AddListener(OnReleaseEvent);
+        }
+    }
+
+    private void OnReleaseEvent(SelectExitEventArgs arg0)
+    {
+        Debug.Log(arg0.interactableObject.transform.gameObject);
+        arg0.interactableObject.transform.gameObject.GetComponent<XRGrabInteractable>().enabled = false;
+        //throw new NotImplementedException();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Block"))
+        {
+            BlockLock block = other.GetComponent<BlockLock>();
+            if (block != null)
+            {
+                block.isInsideSnapZone = false;
+                block.snapZone = null;
+            }
+        }
+    }
+}

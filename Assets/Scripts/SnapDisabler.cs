6e356b0c967bd3186bcbe5132a5bded06474eefa
Kronkh <clara.deniel@brassart.fr>
2026-04-03 09:35:51 +0200

    Push9 - Deplacement
    Deplacement mis en place - à tester

--- Assets/Scripts/SnapDisabler.cs
+++ Assets/Scripts/SnapDisabler.cs
@@ -5,10 +5,14 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 public class SnapDisabler : MonoBehaviour
 {
     public GameObject SnapVolume;
+    public GameObject SnapVolume1;
+    public GameObject SnapVolume2;
 
     private void Awake()
     {
         SnapVolume.SetActive(true);
+        SnapVolume1.SetActive(true);
+        SnapVolume2.SetActive(true);
     }
 
     private void OnTriggerEnter(Collider collider)
@@ -17,6 +21,8 @@ public class SnapDisabler : MonoBehaviour
         {
             Debug.Log("Hiiiii");
             SnapVolume.SetActive(false);
+            SnapVolume1.SetActive(false);
+            SnapVolume2.SetActive(false);
         }
     }
 
@@ -26,6 +32,8 @@ public class SnapDisabler : MonoBehaviour
         {
             Debug.Log("Byeeee");
             SnapVolume.SetActive(true);
+            SnapVolume1.SetActive(true);
+            SnapVolume2.SetActive(true);
         }
     }
 }

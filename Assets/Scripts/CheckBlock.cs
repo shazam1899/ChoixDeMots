e7543563a6fa9456ac013e63a152f0a5956c59a4
Kronkh <tyuiopqsdf8@gmail.com>
2026-04-29 14:18:51 +0200

    Push 101 - Upgrade
    Upgrade script NoblockEffect (gameobject to list gameobject)

--- Assets/Scripts/CheckBlock.cs
+++ Assets/Scripts/CheckBlock.cs
@@ -7,11 +7,16 @@ public class CheckBlock : MonoBehaviour
     [SerializeField] private float t;
 
     [Header("Props")]
-    [SerializeField] private GameObject BadPropsHere1;
-    [SerializeField] private GameObject BadPropsHere2;
-    [SerializeField] private GameObject BadPropsHere3;
-    [SerializeField] private GameObject BadPropsHere4;
-    [SerializeField] private GameObject BadPropsHere5;
+    [SerializeField] private GameObject[] BadPropsHere1;
+    [SerializeField] private GameObject[] BadPropsHere2;
+    [SerializeField] private GameObject[] BadPropsHere3;
+    [SerializeField] private GameObject[] BadPropsHere4;
+    [SerializeField] private GameObject[] BadPropsHere5;
+    [SerializeField] private GameObject[] BadPropsHere6;
+    [SerializeField] private GameObject[] BadPropsHere7;
+    [SerializeField] private GameObject[] BadPropsHere8;
+    [SerializeField] private GameObject[] BadPropsHere9;
+    [SerializeField] private GameObject[] BadPropsHere10;
 
     [Header("Audio")]
     [SerializeField] private GameObject AudioNorm;
@@ -21,9 +26,11 @@ public class CheckBlock : MonoBehaviour
     private float startTime;
     private bool wasBlockActive;
 
+    // Step control
+    private bool step1Done, step2Done, step3Done, step4Done, step5Done;
+
     void Update()
     {
-
         bool isActive = Block.activeInHierarchy;
 
         if (isActive)
@@ -36,7 +43,6 @@ public class CheckBlock : MonoBehaviour
             }
 
             t = Time.time - startTime;
-
             HandleSequence();
         }
         else
@@ -51,36 +57,49 @@ public class CheckBlock : MonoBehaviour
 
     void HandleSequence()
     {
-        if (t > 0f && !BadPropsHere1.activeSelf)
+        if (t > 0f && !step1Done)
         {
-            BadPropsHere1.SetActive(true);
+            SetActiveArray(BadPropsHere1, false);
+            SetActiveArray(BadPropsHere6, true);
+            step1Done = true;
             Debug.Log("Commence");
         }
 
-        if (t > 5f && !BadPropsHere2.activeSelf)
+        if (t > 5f && !step2Done)
         {
-            BadPropsHere2.SetActive(true);
+            SetActiveArray(BadPropsHere2, false);
+            SetActiveArray(BadPropsHere7, true);
+            step2Done = true;
         }
 
-        if (t > 15f && !AudiDistordu1.activeSelf)
+        if (t > 15f && !step3Done)
         {
             AudioNorm.SetActive(false);
             AudiDistordu1.SetActive(true);
-            BadPropsHere3.SetActive(true);
+
+            SetActiveArray(BadPropsHere3, false);
+            SetActiveArray(BadPropsHere8, true);
+
+            step3Done = true;
             Debug.Log("Distorsion1");
-            
         }
 
-        if (t > 20f && !BadPropsHere4.activeSelf)
+        if (t > 20f && !step4Done)
         {
-            BadPropsHere4.SetActive(true);
+            SetActiveArray(BadPropsHere4, false);
+            SetActiveArray(BadPropsHere9, true);
+            step4Done = true;
         }
 
-        if (t > 25f && !AudiDistordu2.activeSelf)
+        if (t > 25f && !step5Done)
         {
             AudiDistordu1.SetActive(false);
             AudiDistordu2.SetActive(true);
-            BadPropsHere5.SetActive(true);
+
+            SetActiveArray(BadPropsHere5, false);
+            SetActiveArray(BadPropsHere10, true);
+
+            step5Done = true;
             Debug.Log("Distorsion2");
         }
     }
@@ -91,11 +110,36 @@ public class CheckBlock : MonoBehaviour
         AudiDistordu1.SetActive(false);
         AudiDistordu2.SetActive(false);
 
-        BadPropsHere1.SetActive(false);
-        BadPropsHere2.SetActive(false);
-        BadPropsHere3.SetActive(false);
-        BadPropsHere4.SetActive(false);
-        BadPropsHere5.SetActive(false);
+        SetActiveArray(BadPropsHere1, true);
+        SetActiveArray(BadPropsHere6, false);
+
+        SetActiveArray(BadPropsHere2, true);
+        SetActiveArray(BadPropsHere7, false);
+
+        SetActiveArray(BadPropsHere3, true);
+        SetActiveArray(BadPropsHere8, false);
+
+        SetActiveArray(BadPropsHere4, true);
+        SetActiveArray(BadPropsHere9, false);
+
+        SetActiveArray(BadPropsHere5, true);
+        SetActiveArray(BadPropsHere10, false);
+
+        //Reset steps
+        step1Done = false;
+        step2Done = false;
+        step3Done = false;
+        step4Done = false;
+        step5Done = false;
+    }
+
+    void SetActiveArray(GameObject[] array, bool state)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] != null)
+                array[i].SetActive(state);
+        }
     }
 }
 

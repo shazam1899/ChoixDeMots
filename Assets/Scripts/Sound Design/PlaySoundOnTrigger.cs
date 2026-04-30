31ea68609d5d388436a3a7a4ebe1fb729795e746
Lefty303 <tristan.sodore@brassart.fr>
2026-04-30 12:44:26 +0200

    Push 124 - Update code SoundTrigger
    Renommé le code PlaySoundTrigger pour s'assurer qu'il marche bien

--- Assets/Scripts/Sound Design/PlaySoundOnTrigger.cs
+++ Assets/Scripts/Sound Design/PlaySoundOnTrigger.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlaySoundOnTrigger : MonoBehaviour
 {
     public AudioClip clip;
     private AudioSource source;
     public string targetTag;
 
     public bool useVelocity = true;
-    public float minVelocity = 0;
+     public float minVelocity = 0;
     public float maxVelocity = 2;
 
     public bool randomizePitch = true;

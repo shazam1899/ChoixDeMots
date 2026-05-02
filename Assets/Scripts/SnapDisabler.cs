91cd70ede7a07aeb67e752f493177c6222f9664e
Kronkh <clara.deniel@brassart.fr>
2026-05-02 23:29:29 +0200

    Push - 149 FixDisable

--- Assets/Scripts/SnapDisabler.cs
+++ Assets/Scripts/SnapDisabler.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class SnapDisabler : MonoBehaviour
 {
-    public GameObject snapVolume;
+    public GameObject[] snapVolume;
     private bool playerInside = false;
+
+    private int insideCount = 0;
 
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.CompareTag("Player"))
         {
-            playerInside = true;
-            snapVolume.SetActive(false);
+            insideCount++;
+
+            if (insideCount == 1)
+            {
+                Debug.Log("bb");
+                foreach (var obj in snapVolume)
+                    obj?.SetActive(false);
+            }
         }
     }
 
@@ -18,8 +26,16 @@ public class SnapDisabler : MonoBehaviour
     {
         if (collider.CompareTag("Player"))
         {
-            playerInside = false;
-            snapVolume.SetActive(true);
+            insideCount--;
+
+            if (insideCount <= 0)
+            {
+                Debug.Log("donne-moi ton adresse");
+                insideCount = 0;
+
+                foreach (var obj in snapVolume)
+                    obj?.SetActive(true);
+            }
         }
     }
 

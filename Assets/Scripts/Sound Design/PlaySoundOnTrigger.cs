e1923556e6a3e639fd61f10f276b1db0b2312532
Lefty303 <tristan.sodore@brassart.fr>
2026-05-06 10:18:59 +0200

    Push 215 - Attempt Fix Son
    - Nouveau Script du SoundOnTrigger

--- Assets/Scripts/Sound Design/PlaySoundOnTrigger.cs
+++ Assets/Scripts/Sound Design/PlaySoundOnTrigger.cs
@@ -7,45 +7,39 @@ public class PlaySoundOnTrigger : MonoBehaviour
     public string targetTag;
 
     public bool useVelocity = true;
-     public float minVelocity = 0;
+    public float minVelocity = 0;
     public float maxVelocity = 2;
 
     public bool randomizePitch = true;
     public float minPitch = 0.8f;
     public float maxPitch = 1.2f;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         source = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
-       if(other.CompareTag(targetTag))
+        if (!other.CompareTag(targetTag))
+            return;
+
+        if (randomizePitch)
+            source.pitch = Random.Range(minPitch, maxPitch);
+
+        float volume = 1f;
+
+        if (useVelocity)
         {
             VelocityEstimator estimator = other.GetComponent<VelocityEstimator>();
-            if(estimator && useVelocity)
+            if (estimator)
             {
                 float v = estimator.GetVelocityEstimate().magnitude;
-                float volume = Mathf.InverseLerp(minVelocity, maxVelocity, v);
-
-                if(randomizePitch)
-                    source.pitch = Random.Range(minPitch, maxPitch);
-
-                source.PlayOneShot(clip,volume);
+                volume = Mathf.InverseLerp(minVelocity, maxVelocity, v);
             }
-            else
-            {
-                if(randomizePitch)
-                    source.pitch = Random.Range(minPitch, maxPitch);
-
-            }
-
-
-            source.PlayOneShot(clip);
         }
+
+        source.PlayOneShot(clip, volume);
     }
 }
+
